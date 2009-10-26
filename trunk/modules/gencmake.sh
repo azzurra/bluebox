1f36381604628d326e2f63ece691bcd2b2ccf609
Matteo Panella <morpheus@azzurra.org>
2009-10-26 01:32:01 +0100

    Completed refactoring of modules/CMakeLists.txt.

--- trunk/modules/gencmake.sh
+++ trunk/modules/gencmake.sh
@@ -1,32 +1,20 @@
 #!/usr/local/bin/bash
 
-#ADD_LIBRARY(m_die MODULE core/m_die.c)
-#SET_TARGET_PROPERTIES(m_die PROPERTIES VERSION 4.2.0 SOVERSION 4 PREFIX "")
-#INSTALL(TARGETS m_die DESTINATION lib)
-
 function library_add()
 {
-  echo "ADD_LIBRARY($CURRNAME MODULE ${FILEPREFIX}$FILENAME)"
-  #echo "SET_TARGET_PROPERTIES($CURRNAME PROPERTIES VERSION 4.2.0 SOVERSION 4 PREFIX \"\")"
-  echo "SET_TARGET_PROPERTIES($CURRNAME PROPERTIES PREFIX \"\" SUFFIX \${SHLIBEXT})"
-  echo "INSTALL(TARGETS $CURRNAME DESTINATION modules${MODPATH2})"
-  echo "IF(APPLE)"
-  echo "  TARGET_LINK_LIBRARIES($CURRNAME ratboxcore ratbox)"
-  echo "ENDIF(APPLE)"
-  echo 
+  echo "CREATE_MODULE($CURRNAME ${FILEPREFIX}$FILENAME modules${MODPATH2})"
 }
 
 if [ ! -z "$1" ]; then
   FILEPREFIX="$1"
 else
   MODPATH2="/autoload"
-  fi
+fi
 
 
 for i in *.c; do 
   FILENAME=$i
   CURRNAME=`echo $i | sed -e "s,\.c,,g"`
-  REALNAME=${CURRNAME}.so
   library_add
 done
 
